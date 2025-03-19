e49a11e1e97bac95f755a68e7e0b116314035ac5
Huynh` Le Minh Thanh` <thanhhuynh0106@gmail.com>
2025-03-19 16:10:33 +0700

    add project ver 1

---
+++ app.py
@@ -0,0 +1,45 @@
+#Imports
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+
+#App
+app = Flask(__name__)
+
+#Loads models
+model_path = os.path.join(os.path.dirname(__file__), 'models', 'rf_model_2.pkl')
+vectorizer_path = os.path.join(os.path.dirname(__file__), 'models', 'vectorizer_2.pkl')
+model = joblib.load(model_path)
+vectorizer = joblib.load(vectorizer_path)
+
+#Predict function
+def predict_attack(payload):
+    payload = vectorizer.transform([payload])
+    prediction = model.predict(payload)
+    accuracy = model.predict_proba(payload).max()
+
+    if (prediction == 1):
+        return f"XSS attack detected! (Confidence: {accuracy:.2%})"
+    if (prediction == 2):
+        return f"SQL Injection attack detected! (Confidence: {accuracy:.2%})"
+    return f"No attack detected! (Confidence: {accuracy:.2%})"
+
+
+#Main page
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    if request.method == 'POST':
+        payload = request.form['payload']
+        result = predict_attack(payload)
+    
+    return render_template('index.html', result=result)
+
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+    var resultElement = document.getElementById('result');
+    if (resultElement) {
+        resultElement.style.display = 'none';
+    }
+}, 5000);
