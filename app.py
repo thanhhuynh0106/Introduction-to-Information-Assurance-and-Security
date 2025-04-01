7172601b6323513de13c770451438f6be2ea9a88
Minh Thanh <thanhhuynh0106@gmail.com>
2025-04-01 21:33:46 +0700

    add model ver 3

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
+import warnings
+warnings.filterwarnings('ignore')
 
 #App
 app = Flask(__name__)
 
 #Loads models
-model_path = os.path.join(os.path.dirname(__file__), 'models', 'rf_model_2.pkl')
-vectorizer_path = os.path.join(os.path.dirname(__file__), 'models', 'vectorizer_2.pkl')
+model_path = os.path.join(os.path.dirname(__file__), 'models', 'rf_model_ver3.pkl')
+vectorizer_path = os.path.join(os.path.dirname(__file__), 'models', 'vectorizer_3.pkl')
 model = joblib.load(model_path)
 vectorizer = joblib.load(vectorizer_path)
 
@@ -26,6 +28,11 @@ def predict_attack(payload):
         return f"XSS attack detected! (Confidence: {accuracy:.2%})"
     if (prediction == 2):
         return f"SQL Injection attack detected! (Confidence: {accuracy:.2%})"
+    if (prediction == 4):
+        return f"Command Injection attack detected! (Confidence: {accuracy:.2%})"
+    if (prediction == 3):
+        return f"XML Injection attack detected! (Confidence: {accuracy:.2%})"
+
     return f"No attack detected! (Confidence: {accuracy:.2%})"
 
 
